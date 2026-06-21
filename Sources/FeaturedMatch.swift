2a555d7af2e4d9684fb500dce771a2b55fa61186
bluntentertainment1-arch <bluntentertainment1@gmail.com>
2026-06-21 22:30:33 +0100

    Enhance FeaturedMatch with flags and visibility logic
    Added home and away flags to FeaturedMatch struct and updated isVisible logic.

--- Sources/FeaturedMatch.swift
+++ Sources/FeaturedMatch.swift
@@ -2,25 +2,39 @@ import Foundation
 
 struct FeaturedMatch: Identifiable, Codable {
     let id: String
-    let competition: String
     let homeTeam: String
     let awayTeam: String
+    let homeFlag: String
+    let awayFlag: String
+    let competition: String
     let matchDate: String
-    let headline: String
-    let subheadline: String
+    let isLive: Bool
     let priority: Int
-    let showUntil: Date
     let active: Bool
-
+    
     var isVisible: Bool {
-        active && showUntil > Date()
+        guard active else { return false }
+        if isLive { return true }
+        // Don't show matches that ended more than 1 day ago
+        let formatter = ISO8601DateFormatter()
+        if let date = formatter.date(from: matchDate) {
+            let oneDayAgo = Calendar.current.date(byAdding: .day, value: -1, to: Date())!
+            return date > oneDayAgo
+        }
+        return true
     }
-}
-
-struct EditorialItem: Identifiable, Codable {
-    let id: String
-    let headline: String
-    let body: String
-    let datePosted: String
-    let active: Bool
+    
+    var homeFlagURL: URL? {
+        if homeFlag.hasPrefix("http") {
+            return URL(string: homeFlag)
+        }
+        return URL(string: "https://flagcdn.com/w80/\(homeFlag.lowercased()).png")
+    }
+    
+    var awayFlagURL: URL? {
+        if awayFlag.hasPrefix("http") {
+            return URL(string: awayFlag)
+        }
+        return URL(string: "https://flagcdn.com/w80/\(awayFlag.lowercased()).png")
+    }
 }
