90db9fc1f4aabace0ad3b5af350d330b7584b5fd
bluntentertainment1-arch <bluntentertainment1@gmail.com>
2026-06-21 21:30:11 +0100

    Update competition and correct spreadsheet URLs
    Updated default competition to Champions League and corrected the URLs for excelFeaturedURL and excelConfigURL.

--- Sources/AppConstants.swift
+++ Sources/AppConstants.swift
@@ -8,11 +8,12 @@ enum AppConstants {
     static let appVersion = "1.0.0"
 
     static let footballDataBaseURL = "https://api.football-data.org/v4"
-    static let defaultCompetition = "WC"
+    static let defaultCompetition = "CL"  // Use Champions League (free tier)
 
-    static let excelFeaturedURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTmtdMDK95ZVLkq9pmki8FgNmD2_PeeBzUT5QHrXO6LyyAzddBVrzThp1RmKfo8g-wW9Cw-aQFRL_rI/pub?gid=792685785&single=true&output=csv"
+    // CORRECTED - URLs were swapped
+    static let excelFeaturedURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTmtdMDK95ZVLkq9pmki8FgNmD2_PeeBzUT5QHrXO6LyyAzddBVrzThp1RmKfo8g-wW9Cw-aQFRL_rI/pub?gid=1758911275&single=true&output=csv"
     static let excelEditorialURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTmtdMDK95ZVLkq9pmki8FgNmD2_PeeBzUT5QHrXO6LyyAzddBVrzThp1RmKfo8g-wW9Cw-aQFRL_rI/pub?gid=600659522&single=true&output=csv"
-    static let excelConfigURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTmtdMDK95ZVLkq9pmki8FgNmD2_PeeBzUT5QHrXO6LyyAzddBVrzThp1RmKfo8g-wW9Cw-aQFRL_rI/pub?gid=1758911275&single=true&output=csv"
+    static let excelConfigURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTmtdMDK95ZVLkq9pmki8FgNmD2_PeeBzUT5QHrXO6LyyAzddBVrzThp1RmKfo8g-wW9Cw-aQFRL_rI/pub?gid=792685785&single=true&output=csv"
 
     static let apiPollInterval: TimeInterval = 30
     static let excelRefreshInterval: TimeInterval = 300
