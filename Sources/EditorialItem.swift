0c860256e9c9f586121f4c0ad478c6bd39fd0071
bluntentertainment1-arch <bluntentertainment1@gmail.com>
2026-06-22 01:08:19 +0100

    Add fullContent property to EditorialItem struct

--- Sources/EditorialItem.swift
+++ Sources/EditorialItem.swift
@@ -4,6 +4,7 @@ struct EditorialItem: Identifiable, Codable, Hashable {
     let id: String
     let headline: String
     let body: String
+    let fullContent: String
     let datePosted: String
     let active: Bool
 }
